169359d98f0a59f8b5e8c60258e881f9dac68e0c
robquesada <rockandbob@hotmail.com>
2014-11-18 12:07:11 -0600

    Destroy dependation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
-  has_many :pets
+  has_many :pets, dependent: :destroy
 
   def self.from_omniauth(auth)
+    # uid: User ID
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
